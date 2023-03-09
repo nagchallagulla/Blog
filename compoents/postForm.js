cc5e10282f4c1eadb04726081a339cb99c9ef915
nagarajuchallagulla <99012371+nagchallagulla@users.noreply.github.com>
2023-03-09 23:06:25 +0530

    Create postForm.js

---
+++ compoents/postForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addPost } from '../action/actions';
+import AddPostForm from './AddPostForm';
+
+const AddPost = () => {
+  const dispatch = useDispatch();
+
+  const handleAddPost = (post) => {
+    dispatch(addPost(post));
+  }
+
+  return (
+    <AddPostForm addPost={handleAddPost} />
+  );
+};
+
+export default AddPost;
